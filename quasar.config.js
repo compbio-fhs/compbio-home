eca63365fce77fe129d179d1b6900a7ef6eb5bde
leandro-acosta <leandro.acosta292@hotmail.com>
2024-05-30 19:26:32 +0800

    First Commit

--- quasar.config.js
+++ quasar.config.js
@@ -53,7 +53,7 @@ export default configure((ctx) => {
 
       // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
-      publicPath: '/',
+      //publicPath: '/',
       // analyze: true,
       // env: {},
       // rawDefine: {}
