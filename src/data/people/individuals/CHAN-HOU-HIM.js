12ad3e84389b2bed1911b4780551bb0b1535a290
KirbyChanH <yc48602@um.edu.mo>
2024-11-25 13:39:43 +0800

    Add personal page V1

---
+++ src/data/people/individuals/CHAN-HOU-HIM.js
@@ -0,0 +1,56 @@
+export default {
+    name: 'Kirby, Chan Hou Him',
+    image: 'https://avatars.githubusercontent.com/u/123046910?s=400&v=4',
+    title: 'PhD Student, Macau',
+    role: 'Image Processing and deep learning computational analysis research',
+    personalPage: '',
+  
+    education: [
+      {
+        degree: 'Master of Precision Medicine',
+        field: 'Bioinformatic & Computational Biology',
+        institution: 'University of Macau',
+        location: 'Macau',
+        period: '2021-2023'
+      },
+      {
+        degree: 'Bachelor',
+        field: 'Electrical and Computer Engineering',
+        institution: 'University of Macau',
+        location: 'Macau',
+        period: '2016-2020'
+      }
+    ],
+  
+    research: {
+      interests: [
+        'Alzheimers Disease analysis with bioimaging',
+        'Deep learning model for medical image analysis',
+        'Computational analysis of medical data',
+        'Image Processing for medical data'
+      ],
+      publications: [
+        {
+          title: 'BATseg: Boundary-aware Multiclass Spinal Cord Tumor Segmentation on 3D MRI Scans',
+          authors: ['Hongkang Song', 'Zihui Zhang', 'Yanpeng Zhou', 'Jie Hu', 'Zishuo Wang', 'Hou Him Chan', 'Chon Lok Lei', 'Chen Xu', 'Yu Xin','Bo Yang'],
+          venue: 'ECCV Workshop BioImage Computing',
+          year: 2024,
+          type: 'journal',
+          doi: ''
+        },
+      ]
+    },  
+  
+    contact: {
+      department: 'Faculty of Health Sciences',
+      institution: 'University of Macau',
+      building: 'E12',
+      address: 'University of Macau, Avenida da Universidade, Taipa, Macau',
+      location: 'Macau',
+      room: '3047',
+      telephone: '8822 2952',
+      fax: '',
+      email: 'yc48602@um.edu.mo'
+    }
+  }
+  
