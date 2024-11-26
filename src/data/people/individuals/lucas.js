2f7dcf47911be6edb33bbe084b0b034b22f1f764
yuanzhen-lucas <yc47658@connect.um.edu.mo>
2024-11-26 21:42:55 +0800

    Template - Yuan Zhen closes #13

---
+++ src/data/people/individuals/lucas.js
@@ -0,0 +1,56 @@
+export default {
+    name: 'Lucas Yuan Zhen',
+    image: 'https://avatars.githubusercontent.com/u/68571770?s=400&u=e333ae9e916cd79bcb7292b4fc2fa90c5488e116&v=4',
+    title: 'PhD Student',
+    role: '',
+    personalPage: '',
+  
+    education: [
+      {
+        degree: 'Master of Cell Biology',
+        field: 'Bioinformatic & Cell Fate Regulation',
+        institution: 'Fujian Agriculture and Forestry University',
+        location: 'Fujian, China',
+        period: '2020-2023'
+      },
+      {
+        degree: 'Bachelor',
+        field: 'Basic Science of Medicine',
+        institution: 'Hubei University',
+        location: 'Hubei, China',
+        period: '2016-2020'
+      }
+    ],
+  
+    research: {
+      interests: [
+        'Artificial Intelligence and Human Disease',
+        'Statistical Analysis of Public Health',
+        'Single-cell Technology Research',
+        'Cell Fate and Regeneration'
+      ],
+      publications: [
+        {
+          title: 'Sea-ATI unravels novel vocabularies of plant active cistrome',
+          authors: ['Wen C, Yuan Z, Zhang X, Chen H, Luo L, Li W, Li T, Ma N, Mao F, Lin D, Lin Z, Lin C, Xu T, Lü P, Lin J, Zhu F.'],
+          venue: 'Nucleic Acids Research',
+          year: 2023,
+          type: 'journal',
+          doi: '10.1093/nar/gkad853'
+        }
+      ]
+    },
+  
+    contact: {
+        department: 'Faculty of Health Sciences',
+        institution: 'University of Macau',
+        building: 'E12',
+        address: 'University of Macau, Avenida da Universidade, Taipa, Macau',
+        location: 'Macau',
+        room: '3047',
+        telephone: '+86 15972916819',
+        fax: '',
+        email: 'yc47658@connect.um.edu.mo'
+    }
+  }
+  
